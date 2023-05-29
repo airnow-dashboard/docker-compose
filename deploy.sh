24556d5b3bd07a2403eaf6f13a47ab5261e6a21b
SoloSynth1 <solosynth1@gmail.com>
2023-05-29 11:53:57 -0700

    remove extra folder created in `deploy.sh`

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p dags logs plugins airnow-data
-chmod a+w ./dags ./logs ./plugins ./airnow-data
+mkdir -p dags logs plugins
+chmod a+w ./dags ./logs ./plugins
 docker compose up airflow-init
 cp airnow-dags/*.py ./dags
